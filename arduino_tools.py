19e34fe31d3b101e310e4504212f76b5ceb45b16
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-15 21:13:36 +0100

    calculadoria de Omnios funcionando

--- arduino_tools.py
+++ arduino_tools.py
@@ -1,11 +1,11 @@
 import colorama as color
 ##R = E/I (ohmios = voltios divididos por amperios)
 
-print("    // | |                                                          /__  ___/                           ")
-print("   //__| |     __      ___   /          ( )   __      ___             / /   ___      ___     //  ___    ")
-print("  / ___  |   //  ) ) //   ) / //   / / / / //   ) ) //   ) )         / /  //   ) ) //   ) ) // ((   ) ) ")
-print(" //    | |  //      //   / / //   / / / / //   / / //   / /         / /  //   / / //   / / //   \ \     ")
-print("//     | | //      ((___/ / ((___( ( / / //   / / ((___/ /   _____ / /  ((___/ / ((___/ / // //   ) )")
+print(r"    // | |                                                          /__  ___/                           ")
+print(r"   //__| |     __      ___   /          ( )   __      ___             / /   ___      ___     //  ___    ")
+print(r"  / ___  |   //  ) ) //   ) / //   / / / / //   ) ) //   ) )         / /  //   ) ) //   ) ) // ((   ) ) ")
+print(r" //    | |  //      //   / / //   / / / / //   / / //   / /         / /  //   / / //   / / //   \ \     ")
+print(r"//     | | //      ((___/ / ((___( ( / / //   / / ((___/ /   _____ / /  ((___/ / ((___/ / // //   ) )")
 
 print("")
 print("")
@@ -14,7 +14,9 @@ print("[1] calculadora de resistencia (Ω)")
 
 x = input(">")
 if x=="1":
-	y=input("Voltaje: ")
-	z=input("Amperios: ")
+	V=input("Voltaje: ")
+	A=input("Amperios: ")
 
+	O=float(V)/float(A)
 
+	print(f"{O}Ω")
